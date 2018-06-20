9ebe27078ddfadf95a7959eb333c6c9a7cd81463
Andy Touch <andyt@unity3d.com>
2018-06-20 14:54:04 +0200

    Added new 2018.2 Scenario Graph Examples

---
+++ ShaderGraph_ExampleLibrary_Project/Assets/Scripts/ToonShaderLightSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ToonShaderLightSettings : MonoBehaviour
+{
+
+	private Light light;
+
+	void OnEnable()
+	{
+		light = GetComponent<Light>();
+	}
+	
+	void Update ()
+	{
+		Shader.SetGlobalVector("_ToonLightDirection", -transform.forward);
+		Shader.SetGlobalColor("_ToonLightColor", light.color);
+	}
+}
